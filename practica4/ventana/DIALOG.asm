547d2301688b1d00511ae8ae6ba2de724bbc5477
gerardocf9 <gerardoa.camposf@gmail.com>
2021-07-25 12:22:57 -0400

    correccion menor en el codigo

--- practica4/ventana/DIALOG.asm
+++ practica4/ventana/DIALOG.asm
@@ -5,20 +5,20 @@ option casemap:none
 include \masm32\include\windows.inc
 include \masm32\include\user32.inc
 include \masm32\include\kernel32.inc
-Include \masm32\include\masm32.inc 
+Include \masm32\include\masm32.inc
 
 includelib \masm32\lib\user32.lib
 includelib \masm32\lib\kernel32.lib
-IncludeLib  \masm32\lib\masm32.lib 
+IncludeLib  \masm32\lib\masm32.lib
 
 WinMain proto :DWORD,:DWORD,:DWORD,:DWORD
 DlgProc PROTO :HWND, :DWORD, :DWORD, :DWORD
 
 
-Get_Input Macro prompt:REQ,buffer:REQ 
-	Invoke StdOut,ADDR prompt 
-	Invoke StdIn,ADDR buffer, LengthOf buffer 
-EndM 
+Get_Input Macro prompt:REQ,buffer:REQ
+	Invoke StdOut,ADDR prompt
+	Invoke StdIn,ADDR buffer, LengthOf buffer
+EndM
 
 
 .data
@@ -30,14 +30,11 @@ TestString db "Hello, everybody",0
 hwndDlg dd 0            ; Handle to the dialog box
 
 
-MsgCaption      db  "Ventana de mensaje",0 
-FileName    db 'test.txt',0     ; file to read
-
 .data?
 hInstance HINSTANCE ?
 CommandLine LPSTR ?
 
-inbuf DB 100 DUP (?) 
+inbuf DB 100 DUP (?)
 hFile       dd ?
 FileSize    dd ?
 hMem        dd ?
@@ -57,7 +54,7 @@ start:
 	invoke GetCommandLine ;invocacion de terminal, no vienen por defecto
 	invoke WinMain, hInstance,NULL,CommandLine, SW_SHOWDEFAULT;llamada al proceso que lo hace todo
 	invoke ExitProcess,eax
-	
+
 ;proceso que lo hace todo...
 WinMain proc hInst:HINSTANCE,hPrevInst:HINSTANCE,CmdLine:LPSTR,CmdShow:DWORD;guardando valores iniciales, pasados en invoke
 	;fijando variables locales
@@ -78,17 +75,17 @@ WinMain proc hInst:HINSTANCE,hPrevInst:HINSTANCE,CmdLine:LPSTR,CmdShow:DWORD;gua
 	invoke LoadIcon,NULL,IDI_APPLICATION ;para el icono de la ventana
 	mov   wc.hIcon,eax
 	mov   wc.hIconSm,eax
-	invoke LoadCursor,NULL,IDC_ARROW ;estilo del cursor 
+	invoke LoadCursor,NULL,IDC_ARROW ;estilo del cursor
 	mov   wc.hCursor,eax
 	invoke RegisterClassEx, addr wc ;fijacion de registros-datos con la direccion-informacion de la clase wc (windows class)
-	
+
 	;utilizacion de la api de windows para crear, sin mostrar ni cargar una ventana, donde la info viene de la clase llenada arriba
-	INVOKE CreateWindowEx,WS_EX_CLIENTEDGE,ADDR ClassName,ADDR AppName,\ 
+	INVOKE CreateWindowEx,WS_EX_CLIENTEDGE,ADDR ClassName,ADDR AppName,\
            WS_OVERLAPPEDWINDOW,CW_USEDEFAULT,\
            CW_USEDEFAULT,300,200,NULL,NULL,\
            hInst,NULL
 	mov   hwnd,eax ;direccion de la ventana
-	;api de windows para 
+	;api de windows para
 	INVOKE ShowWindow, hwnd,SW_SHOWNORMAL ;mostrar la ventana
 	INVOKE UpdateWindow, hwnd ;actualizar la ventana
 	;ciclo "infinito" usado para mantener el programa corriendo y hacer una especie de framework manual, donde dependiendo de los
@@ -142,37 +139,37 @@ DlgProc PROC hWnd:HWND,iMsg:DWORD,wParam:WPARAM, lParam:LPARAM
 			.if eax==IDC_EXIT
 				invoke SendMessage,hWnd,WM_CLOSE,NULL,NULL
 			.elseif eax==IDC_BUTTON
-			
+
 				invoke GetDlgItemText, hWnd,IDC_EDIT,addr inbuf,2000;lee el texto del input box, maximo de 2000 caracteres
-				
+
 				invoke  CreateFile,ADDR inbuf,GENERIC_READ,0,0,\
-				       OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,0            		
-				mov     hFile,eax			
+				       OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,0
+				mov     hFile,eax
 				;obtencion del tamaño de la fila para pedir memoria dinamica
-				invoke  GetFileSize,eax,0				
+				invoke  GetFileSize,eax,0
 				mov     FileSize,eax
-				inc     eax				
+				inc     eax
 				;pedir memoria dinamica
 				invoke  GlobalAlloc,GMEM_FIXED,eax
-				mov     hMem,eax				
-				add     eax,FileSize				
+				mov     hMem,eax
+				add     eax,FileSize
 				mov     BYTE PTR [eax],0   ; Set the last byte to NULL so that StdOut
-				                   ; can safely display the text in memory.				
+				                   ; can safely display the text in memory.
 				;finalmente se lee la fila
-				invoke  ReadFile,hFile,hMem,FileSize,ADDR BytesRead,0				
+				invoke  ReadFile,hFile,hMem,FileSize,ADDR BytesRead,0
 				;se   cierra la fila
-				invoke  CloseHandle,hFile			
+				invoke  CloseHandle,hFile
 				;se escribe la fila
-				invoke  StdOut,hMem				
+				invoke  StdOut,hMem
 				;MessageBox
-				invoke MessageBox, NULL, hMem, addr FileName, 
-				MB_OK 				
+				invoke MessageBox, NULL, hMem, addr inbuf,
+				MB_OK
 				;se libera la memoria dinamica
-				invoke  GlobalFree,hMem	
-				
+				invoke  GlobalFree,hMem
+
 				;invoke SetDlgItemText,hWnd,IDC_EDIT,ADDR TestString
 			.endif
-		.endif		   
+		.endif
         .else
 		mov eax,FALSE
 		ret
